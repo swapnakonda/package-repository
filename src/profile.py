0c5270fa8ccf2140f28036aa5fd7a77f10eb5150
swapnakonda <swaruswapna331@gmail.com>
2022-03-14 11:34:32 +0100

    'PEP8'

--- src/profile.py
+++ src/profile.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     print(f'event - {event}')
     print('event key object -', event['Records'][0]['s3']['object']['key'])
     object_key_value: str = event['Records'][0]['s3']['object']['key']
-    bucket_name:str = event['Records'][0]['s3']['bucket']['name']
+    bucket_name: str = event['Records'][0]['s3']['bucket']['name']
 
     s3 = boto3.resource('s3')
     obj = s3.get_object(Bucket=bucket_name, Key=object_key_value)
