650749f96c641dd37436e0df126cb5aa9f129d91
swapnakonda <swaruswapna331@gmail.com>
2022-03-31 21:15:22 +0200

    updated sorce code with documentation

--- src/get_profile_lambda.py
+++ src/get_profile_lambda.py
@@ -1,5 +1,6 @@
 from src.exceptions.custom_exceptions import EventInputValidationException
 from src.profile_business import get_profile_based_on_email
+from http import HTTPStatus
 
 
 def construct_response(status_code: int, status_message: str, payload: list):
@@ -12,22 +13,32 @@ def construct_response(status_code: int, status_message: str, payload: list):
 
 
 def lambda_handler(event, context):
+    """
+    This lambda gives a profile based on the email given
+    :param event: We take the email in the event as input
+    :param context:
+    :return: We send success along with profile data  when the profile is present in profile table
+            200 - Success
+            400 - BadRequest
+            500 - InternalServerError
+    """
     try:
+        # input email reading
         email_value = event["queryStringParameters"]["email"]
         if email_value is None:
             print('Input email is none')
             raise EventInputValidationException("invalid email")
+
         # BusinessLayer
         sent_response = get_profile_based_on_email(email_value)
+
+        # validations
         if sent_response is not None:
             print(f'record found for the {email_value} given')
-        return construct_response(200,f'data found',sent_response)
+        return construct_response(HTTPStatus.OK, f'data found', sent_response)
     except EventInputValidationException as ex:
         print('EventInputValidationException Occurred - ', ex.args)
-        return construct_response(400,f'validation failed-{ex.args}',[])
+        return construct_response(HTTPStatus.BAD_REQUEST, f'validation failed-{ex.args}', [])
     except Exception as e:
         print('Mother exception as thrown -', e.args)
-        return construct_response(500, 'Internal Server Error', [])
-
-        
-      
+        return construct_response(HTTPStatus.INTERNAL_SERVER_ERROR, 'Internal Server Error', [])
--- src/put_profile_lambda.py
+++ src/put_profile_lambda.py
@@ -1,4 +1,5 @@
 import csv
+from http import HTTPStatus
 
 import boto3
 
@@ -7,40 +8,65 @@ from src.exceptions.custom_exceptions import EventInputValidationException
 
 
 def lambda_handler(event, context):
+    """
+    This lambda process the csv into profile table.
+    :param event:
+    :param context:
+    :return:
+           200 - Success
+           400 - BadRequest
+           500 - InternalServerError
+    """
     print(f'event - {event}')
     try:
-        print('event key object -', event['Records'][0]['s3']['object']['key'])
-        object_key_value: str = event['Records'][0]['s3']['object']['key']
+        # input reading
+        file_name: str = event['Records'][0]['s3']['object']['key']
         bucket_name: str = event['Records'][0]['s3']['bucket']['name']
+
+        print(f'lambda_handler:event file_name - {file_name}')
+        print(f'lambda_handler:event bucket_name - {bucket_name}')
+
+        # validations
         if bucket_name is None:
             print("there is no bucket")
             raise EventInputValidationException("no bucket configuration")
-        if object_key_value is None:
+
+        if file_name is None:
             print("there are no csv files in the bucket")
             raise EventInputValidationException("no csv files")
+
+        # reading data from s3
         s3 = boto3.client('s3')
-        obj = s3.get_object(Bucket=bucket_name, Key=object_key_value)
+        obj = s3.get_object(Bucket=bucket_name, Key=file_name)
         data = obj['Body'].read().decode('utf-8').splitlines()
         if data is None:
-            print("unable to decode the data")
-            raise EventInputValidationException("decoding error")
+            message = 'unable to decode the data'
+            print(f"error occurred {message}")
+            raise EventInputValidationException(message)
         records = csv.DictReader(data)
-        response_list = profile_business.profile_operations(records)
-        if len(records) == len(response_list):
-            final_response =  construct_response(200, f'Successfully processed {object_key_value}', [])
+
+        # business
+        no_of_records, response_list = profile_business.profile_operations(records)
+        print('lambda_handler:no_of_records - ', no_of_records)
+        print('lambda_handler:response_list - ', response_list)
+
+        if no_of_records == len(response_list):
+            final_response = construct_response(HTTPStatus.OK, f'Successfully processed {file_name}', [])
         else:
-            final_response = construct_response(500, f'Error occurred while  processing {object_key_value}', [])
+            final_response = construct_response(HTTPStatus.INTERNAL_SERVER_ERROR,
+                                                f'Error occurred while  processing {file_name}', [])
+
     except EventInputValidationException as obj:
         print('EventInputValidationException occurred - ', obj.args)
-        final_response = construct_response(400, 'Validation failed',[])
+        final_response = construct_response(HTTPStatus.BAD_REQUEST, 'Validation failed', [])
 
     return final_response
 
 
-def construct_response(code: str, message: str, payload: list):
+def construct_response(code: int, message: str, payload: list):
     final_response = {
         'status_code': code,
         'status_message': message,
-        'payload':payload
+        'payload': payload
     }
     return final_response
